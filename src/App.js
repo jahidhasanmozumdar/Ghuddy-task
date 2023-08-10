d1fc4a219844def5063f39ce0eec362f7ca3a289
jahidhasanmozumdar <jahidhasanmozumdar9@gmail.com>
2023-08-10 17:36:06 +0600

    card desgin edit

--- src/App.js
+++ src/App.js
@@ -46,13 +46,13 @@ function App() {
       setActiveFilter(value);
     }
   };
-  console.log(activeFilter);
+
   return (
     <>
       <div className="max-w-[1024px] xl:max-w-[1100px] xxl:max-w-[1440px] xxl:px-[17rem] mx-auto">
-        <h1 className="text-3xl font-bold">Tours</h1>
-        <div className="flex flex-row justify-center items-center mt-[50px] ">
-          <div className="flex justify-center items-center gap-[50px]">
+        <h1 className="text-[32px] font-medium">Tours</h1>
+        <div className="flex flex-row justify-start items-center mt-[16px] mb-[16px] ">
+          <div className="flex justify-center items-center gap-[24px] min-h-[62px]">
             {buttonValue?.map((buttons) => (
               <button
                 onClick={() => handleClick(buttons.value)}
@@ -63,57 +63,63 @@ function App() {
                     : ""
                 }`}
               >
-                <div className="flex flex-col justify-center items-center">
-                  <img className="w-6 h-6 mr-2" src={buttons.iconUrl} alt="" />
-                  <p>{buttons.label}</p>
+                <div className="flex flex-col justify-start items-center w-[64px] h-[58px] md:w-[64px] md:h-[58px] lg:w-[78px] lg:h-[60px]">
+                  <img
+                    className="w-[20px] h-[20px] mr-2"
+                    src={buttons?.iconUrl}
+                    alt=""
+                  />
+                  <p className="text-[#142B33] text-[12px]">{buttons?.label}</p>
                 </div>
               </button>
             ))}
           </div>
         </div>
-        <div>
-          <h1 className="text-3xl">Top destination</h1>
-          <span class=" flex justify-between pb-[8px] ml-[1rem] mt-6">
-            <svg
-              id="sliderButtonPrev"
-              class="cursor-pointer swiper-button-disabled"
-              width="30"
-              height="6"
-              viewBox="0 0 30 6"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M29 3.5C29.2761 3.5 29.5 3.27614 29.5 3C29.5 2.72386 29.2761 2.5 29 2.5L29 3.5ZM0 3L5 5.88675L5 0.113251L0 3ZM29 2.5L4.5 2.5L4.5 3.5L29 3.5L29 2.5Z"
-                fill="#868686"
-              ></path>
-            </svg>
-            <svg
-              id="sliderButtonNext"
-              class="cursor-pointer"
-              width="30"
-              height="6"
-              viewBox="0 0 30 6"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M1 2.5C0.723858 2.5 0.5 2.72386 0.5 3C0.5 3.27614 0.723858 3.5 1 3.5V2.5ZM30 3L25 0.113249V5.88675L30 3ZM1 3.5H25.5V2.5H1V3.5Z"
-                fill="#4E90A8"
-              ></path>
-            </svg>
-          </span>
+        <div className="">
+          <div>
+            <h1 className="text-3xl">Top destination</h1>
+            <span className=" flex justify-between pb-[8px] mt-[16px] mb-[16px]">
+              <svg
+                id="sliderButtonPrev"
+                className="cursor-pointer swiper-button-disabled"
+                width="30"
+                height="6"
+                viewBox="0 0 30 6"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M29 3.5C29.2761 3.5 29.5 3.27614 29.5 3C29.5 2.72386 29.2761 2.5 29 2.5L29 3.5ZM0 3L5 5.88675L5 0.113251L0 3ZM29 2.5L4.5 2.5L4.5 3.5L29 3.5L29 2.5Z"
+                  fill="#868686"
+                ></path>
+              </svg>
+              <svg
+                id="sliderButtonNext"
+                className="cursor-pointer"
+                width="30"
+                height="6"
+                viewBox="0 0 30 6"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1 2.5C0.723858 2.5 0.5 2.72386 0.5 3C0.5 3.27614 0.723858 3.5 1 3.5V2.5ZM30 3L25 0.113249V5.88675L30 3ZM1 3.5H25.5V2.5H1V3.5Z"
+                  fill="#4E90A8"
+                ></path>
+              </svg>
+            </span>
+          </div>
           <div>
             <Swiper
               spaceBetween={2}
-              slidesPerView={1}
+              slidesPerView={3.5}
               navigation
               loop={true}
               className="w-full overflow-hidden my-swiper"
             >
               {hotelData.map((hotels) => (
-                <SwiperSlide key={hotels.id}>
-                  <div className="w-[30px] h-auto p-4  rounded-md ">
+                <SwiperSlide key={Math.random()}>
+                  <div className="w-[30px] h-auto p-4  rounded-md">
                     <Card hotels={hotels} />
                   </div>
                 </SwiperSlide>
--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -16,10 +16,13 @@ const Card = (props) => {
   } = props.hotels;
 
   return (
-    <div className="mt-[8px]">
-      <div class="shadow rounded-[1.1rem] bg-white h-[30rem] xxl:h-[47rem] w-[18rem] xxl:w-[22rem] hover:border-[#4E90A8] hover:shadow-xl">
-        <div class="flex justify-center relative overflow-hidden w-[18rem] xxl:w-full h-[15rem] xxl:h-[17.3rem]">
-          <img src={thumb_image} alt="" className="rounded-md" />
+    <div>
+      <div
+        className="shadow rounded-[1.1rem] bg-white h-[381px] w-[252px] md:h-[381px] md:w-[252px] 
+      lg:h-[442px] lg:w-[279px] hover:border-[#4E90A8] hover:shadow-xl"
+      >
+        <div className="flex justify-center relative overflow-hidden min-h-[205px] w-full max-h-[205px]">
+          <img src={thumb_image} alt="" className="rounded-t-[12px]" />
           <div>
             <span className="absolute top-0 left-0 inline-flex mt-[12px]  px-[60px] py-[6px]  z-10 bg-red-500 text-sm font-medium text-white select-none">
               70% <span>Deal</span>
@@ -31,7 +34,7 @@ const Card = (props) => {
         </div>
         <div className="mt-4 px-[8px] ">
           <div className="flex justify-between">
-            <h2 class="font-normal text-base md:text-lg text-gray-800">
+            <h2 className="font-normal text-base md:text-lg text-gray-800">
               {property_name}
             </h2>
             <div className="flex justify-center items-center">
@@ -40,14 +43,47 @@ const Card = (props) => {
             </div>
           </div>
           <div className="flex justify-start items-center mt-[4px]">
-            <LocationOnOutlinedIcon />
-            <p class="mt-2 text-sm text-gray-800 line-clamp-1">
-              {short_address}
-            </p>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="15"
+                viewBox="0 0 14 15"
+                fill="none"
+              >
+                <g clip-path="url(#clip0_260_570)">
+                  <path
+                    d="M12.25 6.33252C12.25 10.4159 7 13.9159 7 13.9159C7 13.9159 1.75 10.4159 1.75 6.33252C1.75 4.94013 2.30312 3.60477 3.28769 2.62021C4.27226 1.63564 5.60761 1.08252 7 1.08252C8.39239 1.08252 9.72774 1.63564 10.7123 2.62021C11.6969 3.60477 12.25 4.94013 12.25 6.33252Z"
+                    stroke="#142B33"
+                    stroke-width="1.2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M7 8.08252C7.9665 8.08252 8.75 7.29902 8.75 6.33252C8.75 5.36602 7.9665 4.58252 7 4.58252C6.0335 4.58252 5.25 5.36602 5.25 6.33252C5.25 7.29902 6.0335 8.08252 7 8.08252Z"
+                    stroke="#142B33"
+                    stroke-width="1.2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_260_570">
+                    <rect
+                      width="14"
+                      height="14"
+                      fill="white"
+                      transform="translate(0 0.500488)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+            </span>
+            <p className="text-sm text-gray-800">{short_address}</p>
           </div>
           <div className="flex justify-start items-center mt-[4px]">
             <HomeOutlinedIcon />
-            <p class="mt-2 text-sm text-gray-800 ">{property_tag}</p>
+            <p className="mt-2 text-sm text-gray-800 ">{property_tag}</p>
           </div>
           <div className="flex flex-col justify-start items-center mt-[4px]">
             <h2>Checkout Price:</h2>
